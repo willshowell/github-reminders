06a92e0cf97148cae5e81b2966e718a7bd2eef55
Will Howell <will.s.howell@gmail.com>
2017-07-17 20:47:02 -0400

    Add a server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+// Get our API routes
+const api = require('./routes/api');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// Set our api routes
+app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist'));
+});
+
+module.exports = app;
