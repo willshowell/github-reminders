203b7949027bb9e2e6e8ff295dcf3b55513762c7
Will Howell <will.s.howell@gmail.com>
2017-07-19 22:45:16 -0400

    Convert user input to api endpoint

--- client/app/pages/new/new.component.ts
+++ client/app/pages/new/new.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-new-reminder',
+  styleUrls: ['new.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: { 'class': 'app-new-reminder' },
   template: `
-    Here you can create a new reminder
+    <md-input-container>
+      <input mdInput #link placeholder="Github Issue or Pull Request" (blur)="validateLink(link.value)">
+    </md-input-container>
+
+    <pre>
+      Set trigger:
+        Issues
+          - Closed
+          - Assigned
+
+        Pull Request
+          - Closed (closed && merge == false)
+          - Merged
+          - Has conflict
+          - Merged and subsequent release
+    </pre>
   `
 })
 export class AppNewReminderPageComponent {
+
+  /** Currently selected route, driven by link input */
+  selectedApiRoute: string;
+
+  validateLink(val: string) {
+    const validGithubLink = /(?:github\.com\/)?(\w+\/\w+)\/(issues|pull)\/(\d+)$/;
+    const match = validGithubLink.exec(val);
+
+    if (match) {
+      if (match[2] === 'pull') {
+        match[2] = 'pulls';
+      }
+
+      this.selectedApiRoute = `https://api.github.com/repos/${match[1]}/${match[2]}/${+match[3]}`;
+    } else {
+
+    }
+
+  }
 
 }
--- client/app/pages/new/new.module.ts
+++ client/app/pages/new/new.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { MdInputModule } from '@angular/material';
 import { AppNewReminderPageComponent } from './new.component';
 
 @NgModule({
+  imports: [
+    MdInputModule
+  ],
   declarations: [AppNewReminderPageComponent],
   exports: [AppNewReminderPageComponent]
 })
